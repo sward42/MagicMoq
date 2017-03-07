d865b47c410a71446b06636b542543081e4c2b3e
Jurnell Cockhren <jurnell@sophicware.com>
2017-03-07 13:29:27 -0800

    Add project files.

---
+++ MagicMoq/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MagicMoq.MvcApplication" Language="C#" %>
---
+++ MagicMoq/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MagicMoq.Startup))]
+namespace MagicMoq
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
